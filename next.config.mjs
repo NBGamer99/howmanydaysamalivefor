7eaeeb4a19fe70ba1743d6f158473da458663155
nbgamer99 <nabouziyasser80@gmail.com>
2024-06-16 18:48:07 +0100

    chore: Update paths and configuration for GitHub workflows and image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: 'export',
+	output: "export",
 	reactStrictMode: true,
+	images: {
+		unoptimized: true,
+	},
+	basePath: "/",
 };
 
 export default nextConfig;
